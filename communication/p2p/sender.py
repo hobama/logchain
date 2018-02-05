5ba3815768dbaedb35c11985b9a10cad1ecf3345
jaesunkim <jskim.faz@gmail.com>
2018-02-06 02:56:50 +0900

    modified to send to all peers except the peer itself

--- communication/p2p/sender.py
+++ communication/p2p/sender.py
@@ -87,14 +87,17 @@ def send_to_all(p_msg):
 
 # Send to all peers in ConnectedPeerList
 def send_to_all_peers(p_msg, p_port):
-    logging.debug("Send to all peers in ConnectedPeerList")
+    logging.debug("Send msg to all peers in ConnectedPeerList.")
     for peer in peerproperty.nodeproperty.ConnectedPeerList:
-        try:
-            send(peer[1],p_msg, p_port)
-        except Exception as e:
-            print(e)
-        logging.debug("test-connectorpeerlist "+ peer[0])
-        logging.debug("test-connectorpeerlist " + peer[1])
+        if peer[1] == peerproperty.nodeproperty.My_IP_address:
+            logging.debug("Do not send msg it to peer itself.")
+        else:
+            try:
+                send(peer[1],p_msg, p_port)
+            except Exception as e:
+                print(e)
+            logging.debug("test-connectorpeerlist "+ peer[0])
+            logging.debug("test-connectorpeerlist " + peer[1])
 
 
 
