991e02b0c9a70c84f00426985836c64d8f104c68
ldh1428a <ldh1428a@naver.com>
2018-03-21 18:54:15 +0900

    contract_manager_update_1

--- smartcontract/contractmanager.py
+++ smartcontract/contractmanager.py
@@ -1,6 +1,11 @@
 import json
 import threading
+import pickle
+import time
 from queue import Queue
+
+CONTRACT_ADDR = "_ContractStorage."
+SOURCE_ADDR = "SmartContractManager.Sources."
 
 contract_transaction_queue = Queue()
 
@@ -11,21 +16,59 @@ class ContractManager(threading.Thread):
 
     def run(self):
         while True:
-            if contract_transaction_queue.qsize() > 0:
-                transaction = contract_transaction_queue.get()
+            transaction = contract_transaction_queue.get()
 
-                datas = json.dump()
+            datas = json.dump(transaction, default=lambda o: o.__dict__, sort_keys=True)
 
-                if datas['type'] == 'CT':
-                    # CT TYPE 시퀀스
-                    pass
+            time_stamp = time.strftime('%Y%m%d%H%M%S', time.localtime())
 
-                elif datas['type'] == 'RT':
-                    # RT TYPE 시퀀스
-                    pass
+            if datas['type'] == 'CT':
+                # CT TYPE 시퀀스
+                self.deploy_contract(time_stamp, 'example.py', 10)
 
-    def deploy_contract(self, filepath):
-        pass
+            elif datas['type'] == 'RT':
+                # RT TYPE 시퀀스
+                result = self.execute_contract(time_stamp, 'C21341251325312', 'add', 3)
+                print(result)
 
-    def execute_contract(self, c_address):
-        pass
+    def deploy_contract(self, time_stamp, sourcepath, args):
+        contract = getattr(SOURCE_ADDR+sourcepath, 'Contract')(*args)
+        contract_addr = "C"+time_stamp
+
+        f_contract = open(CONTRACT_ADDR + contract_addr, 'wb')
+        pickle.dump(contract, f_contract)
+        f_contract.close()
+
+        f_contract = open(CONTRACT_ADDR + contract_addr, 'rb')
+        state = f_contract.read()
+        f_contract.close()
+
+        print('Contract(' + contract_addr + ') deployed')
+        return {'contractAddr': contract_addr, 'state': state}
+
+    def execute_contract(self, contract_addr, function, args):
+        contractAddress = CONTRACT_ADDR + contract_addr
+        print(contractAddress)
+
+        # read contract file
+        fContract = open(contractAddress, 'r')
+        contract = pickle.load(fContract)
+        print('Contract loaded')
+        method = getattr(contract, function)
+
+        # run method
+        result = method(*args)
+        fContract.close()
+
+        # save state
+        fContract = open(contractAddress, 'w')
+        pickle.dump(contract, fContract)
+        fContract.close()
+
+        # load state
+        fContract = open(contractAddress, 'r')
+        state = fContract.read()
+        fContract.close()
+
+        print('Contract run : result : ' + str(result))
+        return {'result': result, 'state': state}
