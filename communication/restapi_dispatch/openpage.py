68a9bf930c4c4db3e02a3c9364e0e95cdc563a10
Jae Sun Kim <jskim.faz@gmail.com>
2018-03-23 16:04:37 +0900

    Add info pages for client

---
+++ communication/restapi_dispatch/openpage.py
@@ -0,0 +1,28 @@
+
+SavedTxList = [
+    {
+        "tx_addr": "Your transaction address",
+        "tx_title": "The title of your information",
+        "tx_body": "Saved data"
+    }
+]
+
+
+
+DeployedSmartContractList = [
+    {
+        "contract_addr": "Your smart contract address",
+        "contract_title": "The title of your smart contract",
+        "contract_body": "Your smart contract code"
+    }
+]
+
+ExecutedSmartContractList = [
+    {
+        "contract_addr": "Your smart contract address",
+        "contract_title": "The title of your smart contract",
+        "contract_executed_time": "Executed time",
+
+}
+]
+
