35a415299cd0432f51f92075aa2815c018a2ab75
Jae Sun Kim <jskim.faz@gmail.com>
2018-03-26 10:50:22 +0900

    debugging - contract mgr

--- service/blockmanager/block_verify.py
+++ service/blockmanager/block_verify.py
@@ -5,20 +5,20 @@ from smartcontract import contractmanager
 def verify_tx_list(tx_list):
     contract_manager = contractmanager.ContractManager()
     for transaction in tx_list:
-        data = json.dump(transaction, default=lambda o: o.__dict__, sort_keys=True)
+        data = json.dumps(transaction, default=lambda o: o.__dict__, sort_keys=True)
         if data['type'] == 'CT':
-            contract_manager.deploy_contract(data['extra_data']['source'], data['extra_data']['args'])
+            contract_manager.deploy_contract(data['extra_data']['contract_body'], data['extra_data']['contract_args'])
 
         elif data['type'] == 'RT':
-            contract_manager.execute_contract(data['extra_data']['contract_addr'], data['extra_data']['function'], data['extra_data']['args'])
+            contract_manager.execute_contract(data['extra_data']['contract_addr'], data['extra_data']['contract_function'], data['extra_data']['contract_args'])
 
 
 def verify(block):
     contract_manager = contractmanager.ContractManager()
     for transaction in block.tx_list:
-        data = json.dump(transaction, default=lambda o: o.__dict__, sort_keys=True)
+        data = json.dumps(transaction, default=lambda o: o.__dict__, sort_keys=True)
         if data['type'] == 'CT':
-            contract_manager.deploy_contract(data['extra_data']['source'], data['extra_data']['args'])
+            contract_manager.deploy_contract(data['extra_data']['contract_body'], data['extra_data']['contract_args'])
 
         elif data['type'] == 'RT':
-            contract_manager.execute_contract(data['extra_data']['contract_addr'], data['extra_data']['function'], data['extra_data']['args'])
+            contract_manager.execute_contract(data['extra_data']['contract_addr'], data['extra_data']['contract_function'], data['extra_data']['contract_args'])
