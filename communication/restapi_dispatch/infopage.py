5ceb1a6e297c798422519d4f4319f14d46425808
Jae Sun Kim <jskim.faz@gmail.com>
2018-03-26 22:55:15 +0900

    modify info page

--- communication/restapi_dispatch/infopage.py
+++ communication/restapi_dispatch/infopage.py
@@ -2,11 +2,11 @@
 
 # http://host:5000/info/tx/
 SavedTxList = [
-    {
-        "tx_id": "The transaction ID",
-        "tx_from_ip": "The IP of transaction sender",
-        "tx_body": "Saved transaction data"
-    }
+    # {
+    #     "tx_id": "The transaction ID",
+    #     "tx_from_ip": "The IP of transaction sender",
+    #     "tx_body": "Saved transaction data"
+    # }
 ]
 
 def addSavedTx(transaction):
@@ -22,11 +22,11 @@ def addSavedTx(transaction):
 
 # http://host:5000/info/contract/deployed/
 DeployedSmartContractList = [
-    {
-        "smart_contract_tx_id": "The deployed smart contract transaction ID",
-        "smart_contract_tx_from_ip": "The IP of transaction sender",
-        "smart_contract_tx_body": "Deployed smart contract transaction data"
-    }
+    # {
+    #     "smart_contract_tx_id": "The deployed smart contract transaction ID",
+    #     "smart_contract_tx_from_ip": "The IP of transaction sender",
+    #     "smart_contract_tx_body": "Deployed smart contract transaction data"
+    # }
 ]
 
 def addDeployedContract(deployed_contract):
@@ -41,11 +41,11 @@ def addDeployedContract(deployed_contract):
 
 # http://host:5000/info/contract/executed/
 ExecutedSmartContractList = [
-    {
-        "smart_contract_tx_id": "The executed smart contract transaction ID",
-        "smart_contract_tx_from_ip": "The IP of transaction sender",
-        "smart_contract_tx_body": "Executed smart contract transaction data"
-    }
+    # {
+    #     "smart_contract_tx_id": "The executed smart contract transaction ID",
+    #     "smart_contract_tx_from_ip": "The IP of transaction sender",
+    #     "smart_contract_tx_body": "Executed smart contract transaction data"
+    # }
 ]
 
 def addExecutedContract(executed_contract):
