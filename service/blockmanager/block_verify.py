3b891142b98637cb444ab15af1934e1bf1e22cae
Jae Sun Kim <jskim.faz@gmail.com>
2018-03-26 16:59:31 +0900

    debugging - contract execution

--- service/blockmanager/block_verify.py
+++ service/blockmanager/block_verify.py
@@ -19,7 +19,6 @@ def verify_tx_list(tx_list):
                 data_jobj['extra_data']['contract_args']
             )
 
-
         # data = json.dumps(transaction, indent=4, default=lambda o: o.__dict__, sort_keys=True)
         # if data['type'] == 'CT':
         #     contract_manager.deploy_contract(data['extra_data']['contract_body'], data['extra_data']['contract_args'])
