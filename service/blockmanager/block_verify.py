23d03881a90f6a53765f706becbca0e596fa5eb5
Jae Sun Kim <jskim.faz@gmail.com>
2018-03-26 11:59:42 +0900

    debugging - contract mgr

--- service/blockmanager/block_verify.py
+++ service/blockmanager/block_verify.py
@@ -5,12 +5,19 @@ from smartcontract import contractmanager
 def verify_tx_list(tx_list):
     contract_manager = contractmanager.ContractManager()
     for transaction in tx_list:
-        data = json.dumps(transaction, indent=4, default=lambda o: o.__dict__, sort_keys=True)
-        if data['type'] == 'CT':
-            contract_manager.deploy_contract(data['extra_data']['contract_body'], data['extra_data']['contract_args'])
+        data_jobj = json.loads(transaction)
+        if data_jobj['type'] == 'CT':
+            contract_manager.deploy_contract(data_jobj['extra_data']['contract_body'], data_jobj['extra_data']['contract_args'])
+        elif data_jobj['type'] == 'RT':
+            contract_manager.execute_contract(data_jobj['extra_data']['contract_addr'], data_jobj['extra_data']['contract_function'], data_jobj['extra_data']['contract_args'])
 
-        elif data['type'] == 'RT':
-            contract_manager.execute_contract(data['extra_data']['contract_addr'], data['extra_data']['contract_function'], data['extra_data']['contract_args'])
+
+        # data = json.dumps(transaction, indent=4, default=lambda o: o.__dict__, sort_keys=True)
+        # if data['type'] == 'CT':
+        #     contract_manager.deploy_contract(data['extra_data']['contract_body'], data['extra_data']['contract_args'])
+        #
+        # elif data['type'] == 'RT':
+        #     contract_manager.execute_contract(data['extra_data']['contract_addr'], data['extra_data']['contract_function'], data['extra_data']['contract_args'])
 
 
 def verify(block):
