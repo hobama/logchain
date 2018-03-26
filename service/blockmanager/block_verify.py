1a10e19f65ea9ab0fbe3735c5b5f0b6fdd634eac
Jae Sun Kim <jskim.faz@gmail.com>
2018-03-26 22:09:30 +0900

    Add info page

--- service/blockmanager/block_verify.py
+++ service/blockmanager/block_verify.py
@@ -1,6 +1,6 @@
 import json
 from smartcontract import contractmanager
-
+from communication.restapi_dispatch import infopage
 
 def verify_tx_list(tx_list):
     contract_manager = contractmanager.ContractManager()
@@ -12,12 +12,17 @@ def verify_tx_list(tx_list):
                 data_jobj['extra_data']['contract_body'],
                 data_jobj['extra_data']['contract_args']
             )
+            infopage.addDeployedContract(transaction)
         elif data_jobj['type'] == 'RT':
             contract_manager.execute_contract(
                 data_jobj['extra_data']['contract_addr'],
                 data_jobj['extra_data']['contract_function'],
                 data_jobj['extra_data']['contract_args']
             )
+            infopage.addExecutedContract(transaction)
+        elif data_jobj['type'] == 'T':
+            infopage.addSavedTx(transaction)
+
 
         # data = json.dumps(transaction, indent=4, default=lambda o: o.__dict__, sort_keys=True)
         # if data['type'] == 'CT':
