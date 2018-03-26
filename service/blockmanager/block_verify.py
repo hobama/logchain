c4444fc474c0d4c81b84a4b2fe88f8a409ac4f5e
Jae Sun Kim <jskim.faz@gmail.com>
2018-03-26 11:05:39 +0900

    debugging - contract mgr

--- service/blockmanager/block_verify.py
+++ service/blockmanager/block_verify.py
@@ -5,7 +5,7 @@ from smartcontract import contractmanager
 def verify_tx_list(tx_list):
     contract_manager = contractmanager.ContractManager()
     for transaction in tx_list:
-        data = json.dumps(transaction, default=lambda o: o.__dict__, sort_keys=True)
+        data = json.dumps(transaction, indent=4, default=lambda o: o.__dict__, sort_keys=True)
         if data['type'] == 'CT':
             contract_manager.deploy_contract(data['extra_data']['contract_body'], data['extra_data']['contract_args'])
 
@@ -16,7 +16,7 @@ def verify_tx_list(tx_list):
 def verify(block):
     contract_manager = contractmanager.ContractManager()
     for transaction in block.tx_list:
-        data = json.dumps(transaction, default=lambda o: o.__dict__, sort_keys=True)
+        data = json.dumps(transaction, indent=4, default=lambda o: o.__dict__, sort_keys=True)
         if data['type'] == 'CT':
             contract_manager.deploy_contract(data['extra_data']['contract_body'], data['extra_data']['contract_args'])
 
